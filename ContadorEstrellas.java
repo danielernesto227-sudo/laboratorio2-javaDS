07667cf7ce539e03a0a271aa69866bc316232d02
danielernesto227-sudo <danielernesto227@gmail.com>
2026-02-24 15:49:07 -0500

    agregado,interaccion con el usuario

--- ContadorEstrellas.java
+++ ContadorEstrellas.java
@@ -5,14 +5,27 @@ public class ContadorEstrellas {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("¿Cuántas estrellas quieres? ");
+        String repeat;
+        do{
+        System.out.println("Welcome to Andromeda ");
+        System.out.println("¿How many stars do you want?: ");
         int number = sc.nextInt();
 
         for (int i = 1; i <= number; i++) {
             System.out.print("*");
         }
 
+        System.out.print("\nDo you want more stars?(s/n): ");
+        repeat = sc.next();
+         if(repeat.equals("s")){
+            System.out.println("We'll find you some more");       
+        }else{
+            System.out.println("Thank you,you save us stars,jajaja");
+        }
+
+        }while (repeat.contentEquals("s"));
+       
         sc.close();
+        
     }
 }
